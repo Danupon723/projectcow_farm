1ccbdce0fe19f16a94c0fe00db92bae8ac4205a9
Danupon723 <danuponmaneesing@gmail.com>
2026-01-18 18:09:07 +0700

    ฟหก

---
+++ cowfram/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import 'vuetify/style.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+import App from './App.vue'
+
+
+createApp(App).mount('#app')
